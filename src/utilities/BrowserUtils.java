cc1e22322ad6ced7189a4858e516ec3a85308089
Olga <buruiana.olea@gmail.com>
2022-09-01 10:25:31 -0500

    add today git

--- src/utilities/BrowserUtils.java
+++ src/utilities/BrowserUtils.java
@@ -3,5 +3,6 @@ package utilities;
 public class BrowserUtils {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("today git");
     }
 }
